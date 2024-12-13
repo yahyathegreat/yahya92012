c20e26feff1cb05877afccc75677ac56835d8ebc
yahyathegreat <yahyamardan1234@gmail.com>
2024-12-13 20:03:34 +0500

    this shis BORIN

--- main.py
+++ main.py
@@ -9,3 +9,15 @@ def sq(n):
 square = list (map(sq, nums))
 print("square of numbers in lists")
 print(square)
+s1 = {2, 3, 1}
+s2 = {'b', ' a', ' c' }
+s3 =  list(zip(s1, s2))
+list1 = [10, 20, 30, 40]
+list2 = [00, 200, 300, 400]
+for x, y in zip(list1, list2[::-1]):
+    print(x, y)
+    stocks = ['reliance', 'infosys', 'tcs']
+    prices = [2175, 1127, 2750]
+    new_dict = {stocks: prices for stocks,
+                            prices in zip(stocks, prices )}
+print('\n{}'.format(new_dict))   
